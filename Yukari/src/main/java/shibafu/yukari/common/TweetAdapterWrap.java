ece08d68e6d2fd9e808cf331cc1501bca591745d
shibafu <shibafu528@gmail.com>
2013-08-02 13:29:52 +0900

    Home画面のみに絞り、とりあえずツイート受信が行えるようにした

--- Yukari/src/main/java/shibafu/yukari/common/TweetAdapterWrap.java
+++ Yukari/src/main/java/shibafu/yukari/common/TweetAdapterWrap.java
@@ -1,27 +1,50 @@
 package shibafu.yukari.common;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
+import com.loopj.android.image.SmartImageView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import shibafu.yukari.R;
+import shibafu.yukari.twitter.AuthUserRecord;
 import twitter4j.Status;
+import twitter4j.UserMentionEntity;
 
 /**
  * Created by Shibafu on 13/08/01.
  */
 public class TweetAdapterWrap {
     private Context context;
+    private List<AuthUserRecord> userRecords;
     private List<Status> statuses;
     private TweetAdapter adapter;
+    private final SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.JAPAN);
+    private final Pattern viaPattern = Pattern.compile("<a .*>(.+)</a>");
 
-    public TweetAdapterWrap(Context context, List<Status> statuses) {
+    public TweetAdapterWrap(Context context, AuthUserRecord userRecord, List<Status> statuses) {
         this.context = context;
+        this.userRecords = new ArrayList<AuthUserRecord>();
+        userRecords.add(userRecord);
+        this.statuses = statuses;
+        adapter = new TweetAdapter();
+    }
+
+    public TweetAdapterWrap(Context context, List<AuthUserRecord> userRecords, List<Status> statuses) {
+        this.context = context;
+        this.userRecords = userRecords;
         this.statuses = statuses;
         adapter = new TweetAdapter();
     }
@@ -55,7 +78,7 @@ public class TweetAdapterWrap {
             View v = convertView;
 
             if (v == null) {
-                LayoutInflater inflater = LayoutInflater.from(context);
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 v = inflater.inflate(R.layout.raw_tweet, null);
             }
 
@@ -63,8 +86,52 @@ public class TweetAdapterWrap {
             if (st != null) {
                 TextView tvName = (TextView) v.findViewById(R.id.tweet_name);
                 tvName.setText("@" + st.getUser().getScreenName() + " / " + st.getUser().getName());
+                tvName.setTypeface(VLPGothic.getInstance(context).getFont());
                 TextView tvText = (TextView) v.findViewById(R.id.tweet_text);
+                tvText.setTypeface(VLPGothic.getInstance(context).getFont());
                 tvText.setText(st.getText());
+                SmartImageView ivIcon = (SmartImageView)v.findViewById(R.id.tweet_icon);
+                ivIcon.setImageResource(R.drawable.ic_launcher);
+                ivIcon.setImageUrl(st.getUser().getProfileImageURL());
+                TextView tvTimestamp = (TextView)v.findViewById(R.id.tweet_timestamp);
+                Matcher matcher = viaPattern.matcher(st.getSource());
+                String via;
+                if (matcher.find()) {
+                    via = matcher.group(1);
+                }
+                else {
+                    via = st.getSource();
+                }
+                String timestamp = sdf.format(st.getCreatedAt()) + " via " + via;
+                boolean isMention = false;
+                for (UserMentionEntity entity : st.getUserMentionEntities()) {
+                    for (AuthUserRecord aur : userRecords) {
+                        if (aur.ScreenName.equals(entity.getScreenName())) {
+                            isMention = true;
+                            break;
+                        }
+                    }
+                }
+                for (AuthUserRecord aur : userRecords) {
+                    if (aur.ScreenName.equals(st.getInReplyToScreenName())) {
+                        isMention = true;
+                        break;
+                    }
+                }
+                int bgColor = Color.WHITE;
+                if (st.isRetweet()) {
+                    timestamp = "RT by @" + st.getUser().getScreenName() + "\n" + timestamp;
+                    tvName.setText("@" + st.getRetweetedStatus().getUser().getScreenName() + " / " + st.getRetweetedStatus().getUser().getName());
+                    tvText.setText(st.getRetweetedStatus().getText());
+                    ivIcon.setImageUrl(st.getRetweetedStatus().getUser().getProfileImageURL());
+                    bgColor = Color.parseColor("#C2B7FD");
+                }
+                else if (isMention) {
+                    bgColor = Color.parseColor("#EDB3DD");
+                }
+                v.setBackgroundColor(bgColor);
+                v.setTag(bgColor);
+                tvTimestamp.setText(timestamp);
             }
 
             return v;
