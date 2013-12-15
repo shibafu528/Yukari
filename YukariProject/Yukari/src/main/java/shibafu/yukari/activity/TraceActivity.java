7fab0d04db2ff3c932ca72c3845104bf8c227cf2
shibafu <shibafu528@gmail.com>
2013-12-16 01:00:40 +0900

    requestWindowFeatureの書き忘れ

--- YukariProject/Yukari/src/main/java/shibafu/yukari/activity/TraceActivity.java
+++ YukariProject/Yukari/src/main/java/shibafu/yukari/activity/TraceActivity.java
@@ -3,6 +3,7 @@ package shibafu.yukari.activity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
+import android.view.Window;
 
 import shibafu.yukari.R;
 import shibafu.yukari.fragment.TweetListFragment;
@@ -15,6 +16,7 @@ public class TraceActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_parent);
 
         TweetListFragment fragment = new TweetListFragment();
