b001f9f96565616af8e26c37d53ceff091056353
shibafu <shibafu528@gmail.com>
2013-12-17 22:58:09 +0900

    シングルアカウントチューザーを作成した とりあえずマイプロフィールなど飛べるようにした スリープ防止オプションを追加した

---
+++ Yukari/src/main/java/shibafu/yukari/common/SimpleAsyncTask.java
@@ -0,0 +1,9 @@
+package shibafu.yukari.common;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Shibafu on 13/11/23.
+ */
+public abstract class SimpleAsyncTask extends AsyncTask<Void, Void, Void> {
+}
