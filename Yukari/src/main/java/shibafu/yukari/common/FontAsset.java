42acf35f285d7a65e00adc0ef051fe9f773cdba3
shibafu <shibafu528@gmail.com>
2014-01-12 17:45:41 +0900

    font/userfont.ttfが存在している場合そちらをTLフォントとして使用できるようにした

--- Yukari/src/main/java/shibafu/yukari/common/FontAsset.java
+++ Yukari/src/main/java/shibafu/yukari/common/FontAsset.java
@@ -14,6 +14,7 @@ public class FontAsset {
 
     public static final String FONT_NAME = "VL-PGothic-Regular-Mixed4.ttf";
     public static final String FONT_ZIP = "VL-PGothic-Regular-Mixed4.zip";
+    public static final String USERFONT_NAME = "userfont.ttf";
     private static FontAsset instance;
     private Typeface font;
 
@@ -27,7 +28,14 @@ public class FontAsset {
     public static FontAsset getInstance(Context context) {
         if (instance == null) {
             try {
-                instance = new FontAsset(Typeface.createFromFile(getFontFileExtPath(context)));
+                if (checkUserFontExt(context)) {
+                    Log.d("FontAsset", "userfont.ttfを使用します");
+                    instance = new FontAsset(Typeface.createFromFile(getUserFontExtPath(context)));
+                }
+                else {
+                    Log.d("FontAsset", "ビルトインフォントを使用します");
+                    instance = new FontAsset(Typeface.createFromFile(getFontFileExtPath(context)));
+                }
                 if (instance.getFont() == null) {
                     throw new RuntimeException("フォント読み込みに失敗しました");
                 }
@@ -43,14 +51,27 @@ public class FontAsset {
     public static boolean checkFontFileExt(Context context) {
         File dir = new File(context.getExternalFilesDir(null), "font");
         if (!dir.exists()) {
-            dir.mkdir();
+            dir.mkdirs();
         }
 
         return new File(dir, FONT_NAME).exists();
     }
 
+    public static boolean checkUserFontExt(Context context) {
+        File dir = new File(context.getExternalFilesDir(null), "font");
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+
+        return new File(dir, USERFONT_NAME).exists();
+    }
+
     public static File getFontFileExtPath(Context context) {
         return new File(new File(context.getExternalFilesDir(null), "font"), FONT_NAME);
+    }
+
+    public static File getUserFontExtPath(Context context) {
+        return new File(new File(context.getExternalFilesDir(null), "font"), USERFONT_NAME);
     }
 
     public Typeface getFont() {
