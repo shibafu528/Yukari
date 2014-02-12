fbe388bf442d28f1f7db00ee26b145f38163566a
shibafu <shibafu528@gmail.com>
2014-02-12 22:51:10 +0900

    ニコニコ静画サムネイル対応

---
+++ Yukari/src/main/java/shibafu/yukari/media/NicoSeiga.java
@@ -0,0 +1,28 @@
+package shibafu.yukari.media;
+
+/**
+ * Created by Shibafu on 13/12/30.
+ */
+public class NicoSeiga extends LinkMedia {
+
+    public NicoSeiga(String mediaURL) {
+        super(mediaURL);
+    }
+
+    @Override
+    protected String expandMediaURL(String browseURL) {
+        ImageMatch matcher = new ImageMatch("http://(seiga\\.nicovideo\\.jp\\/seiga|nico\\.ms)\\/im(\\d+)", "http://lohas.nicoseiga.jp/thumb/%2l?");
+        return matcher.getFullPageUrl(browseURL);
+    }
+
+    @Override
+    protected String expandThumbURL(String browseURL) {
+        ImageMatch matcher = new ImageMatch("http://(seiga\\.nicovideo\\.jp\\/seiga|nico\\.ms)\\/im(\\d+)", "http://lohas.nicoseiga.jp/thumb/%2i?");
+        return matcher.getFullPageUrl(browseURL);
+    }
+
+    @Override
+    public boolean canPreview() {
+        return true;
+    }
+}
