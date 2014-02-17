c02d9169fa58543eb74384d946acc1093124c5fb
shibafu <shibafu528@gmail.com>
2014-02-17 17:09:40 +0900

    ニコニコ動画・YouTubeのサムネイルに対応

---
+++ YukariProject/Yukari/src/main/java/shibafu/yukari/media/YouTube.java
@@ -0,0 +1,27 @@
+package shibafu.yukari.media;
+
+/**
+ * Created by Shibafu on 13/12/30.
+ */
+public class YouTube extends LinkMedia {
+
+    public YouTube(String mediaURL) {
+        super(mediaURL);
+    }
+
+    @Override
+    protected String expandMediaURL(String browseURL) {
+        return browseURL;
+    }
+
+    @Override
+    protected String expandThumbURL(String browseURL) {
+        ImageMatch matcher = new ImageMatch("^http://(?:www\\.youtube\\.com/watch\\?.*v=|youtu\\.be/)([\\w-]+)", "http://i.ytimg.com/vi/%1/default.jpg");
+        return matcher.getFullPageUrl(browseURL);
+    }
+
+    @Override
+    public boolean canPreview() {
+        return false;
+    }
+}
