ce6d3e1d2e0ee8829af997aca6542b810402a241
shibafu <shibafu528@gmail.com>
2014-03-02 00:47:16 +0900

    Listタブを追加できるようにした

---
+++ Yukari/src/main/java/shibafu/yukari/common/ThrowableAsyncTask.java
@@ -0,0 +1,36 @@
+package shibafu.yukari.common;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by shibafu on 14/03/01.
+ */
+public abstract class ThrowableAsyncTask<Params, Progress, Result>
+        extends AsyncTask<Params, Progress, ThrowableAsyncTask.ThrowableResult<Result>>{
+    public static class ThrowableResult<Result> {
+        private boolean isException;
+        private Exception exception;
+        private Result result;
+
+        public ThrowableResult(Exception e) {
+            this.exception = e;
+            this.isException = true;
+        }
+
+        public ThrowableResult(Result result) {
+            this.result = result;
+        }
+
+        public boolean isException() {
+            return isException;
+        }
+
+        public Exception getException() {
+            return exception;
+        }
+
+        public Result getResult() {
+            return result;
+        }
+    }
+}
