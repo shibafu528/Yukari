91dfaf853608200b6cd662fcbb1654bbc734a557
shibafu <shibafu528@gmail.com>
2014-04-24 00:05:45 +0900

    ミュートを作ろうとした

--- Yukari/src/main/java/shibafu/yukari/activity/MuteActivity.java
+++ Yukari/src/main/java/shibafu/yukari/activity/MuteActivity.java
@@ -1,10 +1,18 @@
 package shibafu.yukari.activity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.ListFragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 import shibafu.yukari.R;
 
@@ -40,5 +48,54 @@ public class MuteActivity extends ActionBarActivity {
 
     public static class InnerFragment extends ListFragment {
 
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setHasOptionsMenu(true);
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            MenuItem addMenu = menu.add(Menu.NONE, R.id.action_add, Menu.NONE, "条件の追加").setIcon(R.drawable.ic_action_add);
+            MenuItemCompat.setShowAsAction(addMenu, MenuItemCompat.SHOW_AS_ACTION_IF_ROOM);
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.action_add: {
+                    MuteConfigDialogFragment dialogFragment = new MuteConfigDialogFragment();
+                    dialogFragment.show(getFragmentManager(), "config");
+                    return true;
+                }
+            }
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public static class MuteConfigDialogFragment extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            View v = getActivity().getLayoutInflater().inflate(R.layout.dialog_mute, null);
+
+            AlertDialog dialog = new AlertDialog.Builder(getActivity())
+                    .setTitle("ミュート設定")
+                    .setView(v)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    })
+                    .setNegativeButton("キャンセル", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    })
+                    .create();
+
+            return dialog;
+        }
     }
 }
