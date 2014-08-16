81ef1ef2a9905846107a73cfc669076f2adb26e0
shibafu <shibafu528@gmail.com>
2014-08-17 07:07:43 +0900

    Android2.3対応

--- YukariProject/Yukari/src/main/java/shibafu/yukari/common/NotificationPreference.java
+++ YukariProject/Yukari/src/main/java/shibafu/yukari/common/NotificationPreference.java
@@ -5,6 +5,7 @@ import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.os.Build;
 import android.preference.DialogPreference;
+import android.preference.PreferenceManager;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -52,7 +53,8 @@ public class NotificationPreference extends DialogPreference{
     protected View onCreateDialogView() {
         LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View view = inflater.inflate(R.layout.dialog_pref_notif, null);
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB &&
+                PreferenceManager.getDefaultSharedPreferences(getContext()).getString("pref_theme", "light").equals("light")) {
             view.setBackgroundColor(Color.WHITE);
         }
         cbEnabled = (CheckBox) view.findViewById(R.id.cbEnableNotif);
