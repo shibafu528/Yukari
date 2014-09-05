8f39a75f7a4df3e18e476e7a68ade357494e869b
shibafu <shibafu528@gmail.com>
2014-09-06 04:46:50 +0900

    スクロールや新着管理、リロードに関するコードの重複を取り除いた

--- Yukari/src/main/java/shibafu/yukari/fragment/tabcontent/SearchListFragment.java
+++ Yukari/src/main/java/shibafu/yukari/fragment/tabcontent/SearchListFragment.java
@@ -1,23 +1,17 @@
 package shibafu.yukari.fragment.tabcontent;
 
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.view.View;
-import android.widget.AbsListView;
-import android.widget.TextView;
 
-import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Set;
 
-import shibafu.yukari.R;
 import shibafu.yukari.common.TabType;
 import shibafu.yukari.twitter.AuthUserRecord;
 import shibafu.yukari.twitter.PRListFactory;
 import shibafu.yukari.twitter.PreformedResponseList;
-import shibafu.yukari.twitter.statusimpl.PreformedStatus;
 import shibafu.yukari.twitter.RESTLoader;
 import shibafu.yukari.twitter.StatusManager;
+import shibafu.yukari.twitter.statusimpl.PreformedStatus;
 import twitter4j.DirectMessage;
 import twitter4j.Query;
 import twitter4j.QueryResult;
@@ -34,11 +28,6 @@ public class SearchListFragment extends TweetListFragment implements StatusManag
     private Query nextQuery;
     private boolean streaming;
 
-    private long lastShowedFirstItemId = -1;
-    private int lastShowedFirstItemY = 0;
-    private View unreadNotifierView;
-    private Set<Long> unreadSet = new HashSet<>();
-
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -54,32 +43,6 @@ public class SearchListFragment extends TweetListFragment implements StatusManag
         if (getMode() == TabType.TABTYPE_TRACK) {
             streaming = true;
         }
-
-        unreadNotifierView = view.findViewById(R.id.unreadNotifier);
-        switch (PreferenceManager.getDefaultSharedPreferences(getActivity()).getString("pref_theme", "light")) {
-            case "light":
-                unreadNotifierView.setBackgroundResource(R.drawable.dialog_full_holo_light);
-                break;
-            case "dark":
-                unreadNotifierView.setBackgroundResource(R.drawable.dialog_full_holo_dark);
-                break;
-        }
-
-        getListView().setOnScrollListener(new AbsListView.OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(AbsListView view, int scrollState) {}
-
-            @Override
-            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-                for (; firstVisibleItem < firstVisibleItem + visibleItemCount && firstVisibleItem < elements.size(); ++firstVisibleItem) {
-                    PreformedStatus status = elements.get(firstVisibleItem);
-                    if (status != null && unreadSet.contains(status.getId())) {
-                        unreadSet.remove(status.getId());
-                    }
-                }
-                updateUnreadNotifier();
-            }
-        });
     }
 
     @Override
@@ -88,7 +51,7 @@ public class SearchListFragment extends TweetListFragment implements StatusManag
         switch (requestMode) {
             case LOADER_LOAD_INIT:
             case LOADER_LOAD_UPDATE:
-                unreadSet.clear();
+                clearUnreadNotifier();
                 restLoader.execute(restLoader.new Params(userRecord, searchQuery));
                 break;
             case LOADER_LOAD_MORE:
@@ -97,17 +60,6 @@ public class SearchListFragment extends TweetListFragment implements StatusManag
                 }
                 break;
         }
-    }
-
-    private void updateUnreadNotifier() {
-        if (unreadSet.size() < 1) {
-            unreadNotifierView.setVisibility(View.INVISIBLE);
-            return;
-        }
-        TextView tv = (TextView) unreadNotifierView.findViewById(R.id.textView);
-        tv.setText(String.format("新着 %d件", unreadSet.size()));
-
-        unreadNotifierView.setVisibility(View.VISIBLE);
     }
 
     @Override
@@ -149,29 +101,6 @@ public class SearchListFragment extends TweetListFragment implements StatusManag
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-        if (lastShowedFirstItemId > -1) {
-            int position;
-            int length = elements.size();
-            for (position = 0; position < length; ++position) {
-                if (elements.get(position).getId() == lastShowedFirstItemId) break;
-            }
-            if (position < length) {
-                listView.setSelectionFromTop(position, lastShowedFirstItemY);
-            }
-        }
-        updateUnreadNotifier();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        lastShowedFirstItemId = listView.getItemIdAtPosition(listView.getFirstVisiblePosition());
-        lastShowedFirstItemY = listView.getChildAt(0).getTop();
-    }
-
-    @Override
     public void onStatus(AuthUserRecord from, final PreformedStatus status, boolean muted) {
         if (users.contains(from) && !elements.contains(status)) {
             if (getMode() == TabType.TABTYPE_MENTION &&
@@ -186,22 +115,7 @@ public class SearchListFragment extends TweetListFragment implements StatusManag
                     getHandler().post(new Runnable() {
                         @Override
                         public void run() {
-                            if (!elements.contains(status)) {
-                                if (position < elements.size()) {
-                                    if (elements.get(position).getId() == status.getId()) return;
-                                }
-                                elements.add(position, status);
-                                int firstPos = listView.getFirstVisiblePosition();
-                                int y = listView.getChildAt(0).getTop();
-                                adapterWrap.notifyDataSetChanged();
-                                if (elements.size() == 1 || firstPos == 0 && y > -1) {
-                                    listView.setSelection(0);
-                                } else {
-                                    unreadSet.add(status.getId());
-                                    listView.setSelectionFromTop(firstPos + 1, y);
-                                }
-                                updateUnreadNotifier();
-                            }
+                            insertElement(status, position);
                         }
                     });
                 }
@@ -219,26 +133,7 @@ public class SearchListFragment extends TweetListFragment implements StatusManag
                 getHandler().post(new Runnable() {
                     @Override
                     public void run() {
-                        Iterator<PreformedStatus> iterator = elements.iterator();
-                        while (iterator.hasNext()) {
-                            if (iterator.next().getId() == status.getId()) {
-                                int firstPos = listView.getFirstVisiblePosition();
-                                long firstId = listView.getItemIdAtPosition(firstPos);
-                                int y = listView.getChildAt(0).getTop();
-                                iterator.remove();
-                                adapterWrap.notifyDataSetChanged();
-                                if (elements.size() == 1 || firstPos == 0) {
-                                    listView.setSelection(0);
-                                } else {
-                                    listView.setSelectionFromTop(firstPos - (firstId < status.getId()? 1 : 0), y);
-                                }
-                                if (unreadSet.contains(status.getId())) {
-                                    unreadSet.remove(status.getId());
-                                    updateUnreadNotifier();
-                                }
-                                break;
-                            }
-                        }
+                        deleteElement(status);
                     }
                 });
                 for (Iterator<PreformedStatus> iterator = stash.iterator(); iterator.hasNext(); ) {
