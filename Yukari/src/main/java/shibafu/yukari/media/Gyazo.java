6c3ee7ed42fa23e37dcc4aa49086a40793b7fbc4
shibafu <shibafu528@gmail.com>
2014-12-22 18:59:32 +0900

    i.gyazo.comの直リンクURLを開こうとするとクラッシュするバグを修正

--- Yukari/src/main/java/shibafu/yukari/media/Gyazo.java
+++ Yukari/src/main/java/shibafu/yukari/media/Gyazo.java
@@ -11,13 +11,13 @@ public class Gyazo extends LinkMedia{
 
     @Override
     protected String expandMediaURL(String browseURL) {
-        ImageMatch matcher = new ImageMatch("http://(?:www\\.)?gyazo\\.com/(\\w+)(?:\\.png)?", "http://gyazo.com/%1.png");
+        ImageMatch matcher = new ImageMatch("http://(?:www\\.|i\\.)?gyazo\\.com/(\\w+)(?:\\.png)?", "http://gyazo.com/%1.png");
         return matcher.getFullPageUrl(browseURL);
     }
 
     @Override
     protected String expandThumbURL(String browseURL) {
-        ImageMatch matcher = new ImageMatch("http://(?:www\\.)?gyazo\\.com/(\\w+)(?:\\.png)?", "http://gyazo.com/thumb/%1.png");
+        ImageMatch matcher = new ImageMatch("http://(?:www\\.|i\\.)?gyazo\\.com/(\\w+)(?:\\.png)?", "http://gyazo.com/thumb/%1.png");
         return matcher.getFullPageUrl(browseURL);
     }
 
