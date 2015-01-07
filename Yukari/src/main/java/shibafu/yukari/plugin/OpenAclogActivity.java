6e884650bee7b0d3840c379ef6622b598b7d27e8
shibafu <shibafu528@gmail.com>
2015-01-07 13:32:37 +0900

    「このツイートのaclogを開く」機能のTSlib対応

--- Yukari/src/main/java/shibafu/yukari/plugin/OpenAclogActivity.java
+++ Yukari/src/main/java/shibafu/yukari/plugin/OpenAclogActivity.java
@@ -5,6 +5,10 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+import twitter.share.lib.TwitterIntent;
+import twitter.share.lib.TwitterShare;
+import twitter.share.lib.intent.StatusIntent;
+
 /**
  * Created by shibafu on 14/03/27.
  */
@@ -13,7 +17,17 @@ public class OpenAclogActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        String id = getIntent().getStringExtra("id");
+        Intent intent = getIntent();
+        String id;
+        switch (intent.getAction()) {
+            case TwitterIntent.ACTION_SHOW_STATUS:
+                StatusIntent statusIntent = (StatusIntent) TwitterShare.INSTANCE.getTwitterIntent(intent);
+                id = String.valueOf(statusIntent.getId());
+                break;
+            default:
+                id = intent.getStringExtra("id");
+                break;
+        }
 
         Uri uri = Uri.withAppendedPath(Uri.parse("http://aclog.koba789.com/i"), id);
         startActivity(new Intent(Intent.ACTION_VIEW, uri));
