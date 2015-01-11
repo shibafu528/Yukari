8016cb2b7c537cd2b893e69c71b37273134e02d3
shibafu <shibafu528@gmail.com>
2015-01-11 23:00:45 +0900

    SNサジェストの高速化
    close #57

--- Yukari/src/main/java/shibafu/yukari/activity/SNPickerActivity.java
+++ Yukari/src/main/java/shibafu/yukari/activity/SNPickerActivity.java
@@ -5,7 +5,11 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,15 +23,16 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
 import shibafu.yukari.R;
-import shibafu.yukari.activity.base.YukariBase;
+import shibafu.yukari.activity.base.FragmentYukariBase;
 import shibafu.yukari.common.bitmapcache.ImageLoaderTask;
 import shibafu.yukari.database.CentralDatabase;
 
-public class SNPickerActivity extends YukariBase {
+public class SNPickerActivity extends FragmentYukariBase implements LoaderManager.LoaderCallbacks<List<SNPickerActivity.SuggestedName>> {
 
     public static final String EXTRA_USER_ID = "user_id";
     public static final String EXTRA_SCREEN_NAME = "screen_name";
@@ -37,7 +42,7 @@ public class SNPickerActivity extends YukariBase {
     private EditText editText;
 
     private Adapter adapter;
-    private ArrayList<Data> dataList;
+    private ArrayList<SuggestedName> suggestedNameList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,15 +59,14 @@ public class SNPickerActivity extends YukariBase {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_snpicker);
 
-        dataList = new ArrayList<>();
-        adapter = new Adapter(this, dataList);
+        adapter = new Adapter(this, suggestedNameList);
 
         ListView listView = (ListView) findViewById(R.id.lvSNPick);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Data d = dataList.get(position);
+                SuggestedName d = suggestedNameList.get(position);
                 Intent intent = new Intent();
                 intent.putExtra(EXTRA_USER_ID, d.id);
                 intent.putExtra(EXTRA_NAME, d.name);
@@ -89,35 +93,14 @@ public class SNPickerActivity extends YukariBase {
     }
 
     private void updateList() {
-        CentralDatabase db = getTwitterService().getDatabase();
-
-        String et = editText.getText().toString();
-        Cursor c;
-        if (et.equals("")) {
-            c = db.getUsersCursor();
+        String query = editText.getText().toString();
+        if (TextUtils.isEmpty(query) || query.length() < 2) {
+            suggestedNameList.clear();
+            adapter.notifyDataSetChanged();
+            tvHead.setText("Suggest");
+        } else {
+            getSupportLoaderManager().restartLoader(0, null, this);
         }
-        else {
-            et = "%" + et + "%";
-            c = db.getUsersCursor(CentralDatabase.COL_USER_NAME + " LIKE ? OR " + CentralDatabase.COL_USER_SCREEN_NAME + " LIKE ?",
-                    new String[]{et, et});
-        }
-
-        final int rowId = c.getColumnIndex(CentralDatabase.COL_USER_ID);
-        final int rowName = c.getColumnIndex(CentralDatabase.COL_USER_NAME);
-        final int rowSN = c.getColumnIndex(CentralDatabase.COL_USER_SCREEN_NAME);
-        final int rowURL = c.getColumnIndex(CentralDatabase.COL_USER_PROFILE_IMAGE_URL);
-
-        dataList.clear();
-
-        if (c.moveToFirst()) {
-            do {
-                Data d = new Data(c.getLong(rowId), c.getString(rowName), c.getString(rowSN), c.getString(rowURL));
-                dataList.add(d);
-            } while (c.moveToNext());
-        }
-        tvHead.setText("Suggest (" + dataList.size() + ")");
-        adapter.notifyDataSetChanged();
-        c.close();
     }
 
     @Override
@@ -126,17 +109,33 @@ public class SNPickerActivity extends YukariBase {
     }
 
     @Override
-    public void onServiceDisconnected() {
+    public void onServiceDisconnected() {}
 
+    @Override
+    public Loader<List<SuggestedName>> onCreateLoader(int id, Bundle args) {
+        return new ScreenNameAsyncLoader(getApplicationContext(),
+                editText.getText().toString(),
+                getTwitterService().getDatabase());
     }
 
-    private class Data {
+    @Override
+    public void onLoadFinished(Loader<List<SuggestedName>> loader, List<SuggestedName> suggestedName) {
+        suggestedNameList.clear();
+        suggestedNameList.addAll(suggestedName);
+        adapter.notifyDataSetChanged();
+        tvHead.setText("Suggest (" + suggestedNameList.size() + ")");
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<SuggestedName>> loader) {}
+
+    static class SuggestedName {
         long id;
         String name;
         String sn;
         String imageURL;
 
-        private Data(long id, String name, String sn, String imageURL) {
+        private SuggestedName(long id, String name, String sn, String imageURL) {
             this.id = id;
             this.name = name;
             this.sn = sn;
@@ -144,11 +143,11 @@ public class SNPickerActivity extends YukariBase {
         }
     }
 
-    private class Adapter extends ArrayAdapter<Data> {
+    private class Adapter extends ArrayAdapter<SuggestedName> {
 
         private LayoutInflater inflater;
 
-        public Adapter(Context context, List<Data> objects) {
+        public Adapter(Context context, List<SuggestedName> objects) {
             super(context, 0, objects);
             inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         }
@@ -170,7 +169,7 @@ public class SNPickerActivity extends YukariBase {
                 vh = (ViewHolder) v.getTag();
             }
 
-            Data d = getItem(position);
+            SuggestedName d = getItem(position);
             if (d != null) {
                 vh.tvName.setText(d.name);
                 vh.tvScreenName.setText("@" + d.sn);
@@ -184,6 +183,54 @@ public class SNPickerActivity extends YukariBase {
             ImageView ivIcon;
             TextView tvScreenName;
             TextView tvName;
+        }
+    }
+
+    private static class ScreenNameAsyncLoader extends AsyncTaskLoader<List<SuggestedName>> {
+        private String query;
+        private WeakReference<CentralDatabase> database;
+
+        public ScreenNameAsyncLoader(Context context, String query, CentralDatabase database) {
+            super(context);
+            this.query = query;
+            this.database = new WeakReference<>(database);
+        }
+
+        @Override
+        public List<SuggestedName> loadInBackground() {
+            List<SuggestedName> suggestedNameList = new ArrayList<>();
+            CentralDatabase db = database.get();
+            if (db != null) {
+                Cursor c;
+                if (query.equals("")) {
+                    c = db.getUsersCursor();
+                } else {
+                    query = "%" + query + "%";
+                    c = db.getUsersCursor(CentralDatabase.COL_USER_NAME + " LIKE ? OR " + CentralDatabase.COL_USER_SCREEN_NAME + " LIKE ?",
+                            new String[]{query, query});
+                }
+                try {
+                    final int rowId = c.getColumnIndex(CentralDatabase.COL_USER_ID);
+                    final int rowName = c.getColumnIndex(CentralDatabase.COL_USER_NAME);
+                    final int rowSN = c.getColumnIndex(CentralDatabase.COL_USER_SCREEN_NAME);
+                    final int rowURL = c.getColumnIndex(CentralDatabase.COL_USER_PROFILE_IMAGE_URL);
+
+                    if (c.moveToFirst()) {
+                        do {
+                            SuggestedName d = new SuggestedName(c.getLong(rowId), c.getString(rowName), c.getString(rowSN), c.getString(rowURL));
+                            suggestedNameList.add(d);
+                        } while (c.moveToNext());
+                    }
+                } finally {
+                    c.close();
+                }
+            }
+            return suggestedNameList;
+        }
+
+        @Override
+        protected void onStartLoading() {
+            forceLoad();
         }
     }
 }
