aef428abce22f84fabc031b860a7a45696e4f846
shibafu <shibafu528@gmail.com>
2015-02-06 20:58:48 +0900

    bbopコマンドの送り先をla0cからnkroidに変更

--- YukariProject/Yukari/src/main/java/shibafu/yukari/activity/TweetActivity.java
+++ YukariProject/Yukari/src/main/java/shibafu/yukari/activity/TweetActivity.java
@@ -415,7 +415,7 @@ public class TweetActivity extends FragmentYukariBase implements DraftDialogFrag
                             inputText = "Javaビームﾋﾞﾋﾞﾋﾞﾋﾞﾋﾞﾋﾞﾋﾞwwwwwwwwww";
                             break;
                         case "::bb":
-                            inputText = input.replace("::bb", "@la0c bbop");
+                            inputText = input.replace("::bb", "@nkroid bbop");
                             break;
                         case "::cn": {
                             String name = inputText.replace("::cn ", "");
