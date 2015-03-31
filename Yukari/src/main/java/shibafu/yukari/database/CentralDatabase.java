75857b6a0e7d5065eb2ae92e9038457ecf18ff92
shibafu <shibafu528@gmail.com>
2015-03-31 13:25:41 +0900

    ブックマークが読めない時は飛ばすようにした

--- Yukari/src/main/java/shibafu/yukari/database/CentralDatabase.java
+++ Yukari/src/main/java/shibafu/yukari/database/CentralDatabase.java
@@ -623,7 +623,14 @@ public class CentralDatabase {
         ArrayList<Bookmark> bookmarks = new ArrayList<>();
         try {
             if (cursor.moveToFirst()) {
-                do bookmarks.add(new Bookmark(cursor));
+                do {
+                    //TODO: デシリアライズ時のt4jバージョン差によるクラッシュを回避する技です。こんなの辞められるようにしような。
+                    try {
+                        bookmarks.add(new Bookmark(cursor));
+                    } catch (RuntimeException e) {
+                        e.printStackTrace();
+                    }
+                }
                 while (cursor.moveToNext());
             }
         } finally {
