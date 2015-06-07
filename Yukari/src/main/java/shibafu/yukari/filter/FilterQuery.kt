dc11620ab64b22bd709770613e107633f311601f
shibafu <shibafu528@gmail.com>
2015-06-07 12:32:59 +0900

    生成検査、微妙なので消した

--- Yukari/src/main/java/shibafu/yukari/filter/FilterQuery.kt
+++ Yukari/src/main/java/shibafu/yukari/filter/FilterQuery.kt
@@ -17,20 +17,9 @@ import twitter4j.TwitterResponse
  *
  * @property sources 抽出対象のデータソースです。
  * @property rootExpression コンパイルされたクエリ式です。評価時に呼び出されます。
- * @constructor 抽出ソースとコンパイルされた式を格納するオブジェクトを生成します。このコンストラクタはクエリコンパイラ以外から呼ばれてはいけません。呼ばれた場合、{@link IllegalAccessError} がスローされます。
+ * @constructor 抽出ソースとコンパイルされた式を格納するオブジェクトを生成します。このコンストラクタはクエリコンパイラ以外呼びだすべきではありません。
  */
 public data class FilterQuery(public val sources: List<FilterSource>, private val rootExpression: Expression) {
-    init {
-        //インスタンス生成元チェック
-        val compilerName = javaClass<QueryCompiler>().getName()
-        val callFrom = Thread.currentThread().getStackTrace()[3].getClassName()
-        if (!compilerName.equals(callFrom)) {
-            Log.e("FilterQuery", "Call from: " + callFrom)
-            Log.e("FilterQuery", "Actual: " + compilerName)
-            throw IllegalAccessError("フィルタシステム外からのインスタンス生成は許可されていません。")
-        }
-    }
-
     /**
      * ツイートやメッセージをコンパイルされたクエリ式で評価します。
      * @param target 評価対象
