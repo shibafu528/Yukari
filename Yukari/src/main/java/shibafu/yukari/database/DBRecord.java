8723e4aa8a4005f407163f1966408f9a3b11e747
shibafu <shibafu528@gmail.com>
2015-06-30 02:00:55 +0900

    getContentValues()のデフォルト実装

--- Yukari/src/main/java/shibafu/yukari/database/DBRecord.java
+++ Yukari/src/main/java/shibafu/yukari/database/DBRecord.java
@@ -2,9 +2,37 @@ package shibafu.yukari.database;
 
 import android.content.ContentValues;
 
+import java.lang.reflect.Field;
+import java.util.Date;
+
 /**
  * Created by Shibafu on 13/12/19.
  */
 public interface DBRecord {
-    ContentValues getContentValues();
+    default ContentValues getContentValues() {
+        ContentValues values = new ContentValues();
+
+        for (Field f : this.getClass().getDeclaredFields()) {
+            DBColumn column = f.getAnnotation(DBColumn.class);
+            if (column != null) {
+                try {
+                    f.setAccessible(true);
+                    Object val = f.get(this);
+                    if (val instanceof String || val instanceof Integer || val instanceof Long || val instanceof Short || val instanceof Float || val instanceof Double) {
+                        if ("id".equals(f.getName()) && val instanceof Long && ((Long) val) == -1) {
+                            continue;
+                        }
+                        values.put(column.value(), val.toString());
+                    } else if (val instanceof Date) {
+                        values.put(column.value(), ((Date) val).getTime());
+                    } else {
+                        throw new IllegalStateException("field cannot put ContentValues: " + f.getName());
+                    }
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return values;
+    }
 }
--- Yukari/src/main/java/shibafu/yukari/database/Template.java
+++ Yukari/src/main/java/shibafu/yukari/database/Template.java
@@ -1,6 +1,5 @@
 package shibafu.yukari.database;
 
-import android.content.ContentValues;
 import android.database.Cursor;
 
 import java.io.Serializable;
@@ -9,36 +8,29 @@ import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Setter;
 
+import static shibafu.yukari.database.CentralDatabase.*;
+
 /**
  * Created by shibafu on 15/06/23.
  */
 @Data
-@DBTable(CentralDatabase.TABLE_TEMPLATE)
+@DBTable(TABLE_TEMPLATE)
 public class Template implements DBRecord, Serializable{
-    @Setter(AccessLevel.NONE) private long id = -1;
-    private String value;
+    @Setter(AccessLevel.NONE)
+    @DBColumn(COL_TEMPLATE_ID) private long id = -1;
+    @DBColumn(COL_TEMPLATE_VALUE) private String value;
 
     public Template(String value) {
         this.value = value;
     }
 
     public Template(Cursor cursor) {
-        this.id = cursor.getLong(cursor.getColumnIndex(CentralDatabase.COL_TEMPLATE_ID));
-        this.value = cursor.getString(cursor.getColumnIndex(CentralDatabase.COL_TEMPLATE_VALUE));
+        this.id = cursor.getLong(cursor.getColumnIndex(COL_TEMPLATE_ID));
+        this.value = cursor.getString(cursor.getColumnIndex(COL_TEMPLATE_VALUE));
     }
 
     @Override
     public String toString() {
         return value;
-    }
-
-    @Override
-    public ContentValues getContentValues() {
-        ContentValues values = new ContentValues();
-        if (id > -1) {
-            values.put(CentralDatabase.COL_TEMPLATE_ID, id);
-        }
-        values.put(CentralDatabase.COL_TEMPLATE_VALUE, value);
-        return values;
     }
 }
