d0bd545445db07eb6e7c3d8ab562c0bb493acf7a
shibafu <shibafu528@gmail.com>
2015-07-01 00:14:29 +0900

    totoridipjp4Jを採用した
    close #71

--- Yukari/src/main/java/shibafu/yukari/media/Totori.java
+++ Yukari/src/main/java/shibafu/yukari/media/Totori.java
@@ -1,12 +1,6 @@
 package shibafu.yukari.media;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import totoridipjp4j.TotoriDipJPFactory;
 
 /**
  * Created by Shibafu on 13/12/30.
@@ -20,41 +14,7 @@ public class Totori extends LinkMedia {
 
     private String getSourceURL(final String browseURL) {
         if (sourceURL == null) {
-            final String[] thumbURL = new String[1];
-            Thread thread = new Thread(() -> {
-                try {
-                    HttpURLConnection conn = (HttpURLConnection) new URL(browseURL).openConnection();
-                    conn.setReadTimeout(10000);
-                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                    try {
-                        String s;
-                        while ((s = br.readLine()) != null) {
-                            if (s.contains("id=\"topimg\"")) {
-                                Pattern pattern = Pattern.compile(".*<img src=\"(.+\\.jpg)\" .*/>.*");
-                                Matcher m = pattern.matcher(s);
-                                if (m.find()) {
-                                    thumbURL[0] = m.group(1).startsWith("http://")? m.group(1) : "http://totori.dip.jp/" + m.group(1);
-                                    return;
-                                }
-                            }
-                        }
-                    } finally {
-                        br.close();
-                        conn.disconnect();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                thumbURL[0] = null;
-            });
-            thread.start();
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            sourceURL = thumbURL[0];
+            sourceURL = new TotoriDipJPFactory().getInstance().getTopImg().getUrl();
         }
         return sourceURL;
     }
