0f5796cb20ec48e406eb19755514d47592fc8c65
shibafu <shibafu528@gmail.com>
2016-05-09 23:47:47 +0900

    QRコード解析中にOOMしたら握りつぶすことにした

--- Yukari/src/main/java/shibafu/yukari/activity/PreviewActivity.java
+++ Yukari/src/main/java/shibafu/yukari/activity/PreviewActivity.java
@@ -532,15 +532,21 @@ public class PreviewActivity extends FragmentYukariBase {
     }
 
     private void processZxing() {
-        int[] pixels = new int[image.getWidth() * image.getHeight()];
-        image.getPixels(pixels, 0, image.getWidth(), 0, 0, image.getWidth(), image.getHeight());
-        LuminanceSource source = new RGBLuminanceSource(image.getWidth(), image.getHeight(), pixels);
-        BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(source));
         try {
-            Result result = new MultiFormatReader().decode(binaryBitmap);
-            llQrText.setVisibility(View.VISIBLE);
-            tvQrText.setText(result.getText());
-        } catch (NotFoundException e) {
+            int[] pixels = new int[image.getWidth() * image.getHeight()];
+            image.getPixels(pixels, 0, image.getWidth(), 0, 0, image.getWidth(), image.getHeight());
+            LuminanceSource source = new RGBLuminanceSource(image.getWidth(), image.getHeight(), pixels);
+            BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(source));
+            try {
+                Result result = new MultiFormatReader().decode(binaryBitmap);
+                llQrText.setVisibility(View.VISIBLE);
+                tvQrText.setText(result.getText());
+            } catch (NotFoundException e) {
+                llQrText.setVisibility(View.GONE);
+            }
+        } catch (OutOfMemoryError e) {
+            // そんなこともある
+            System.gc();
             llQrText.setVisibility(View.GONE);
         }
     }
