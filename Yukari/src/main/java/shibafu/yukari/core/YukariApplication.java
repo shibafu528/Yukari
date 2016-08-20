545f8e06182412fe9d6edcc5f492f8f1ca1b2933
shibafu <shibafu528@gmail.com>
2016-08-20 14:48:45 +0900

    http1.1強制オプションの追加

--- Yukari/src/main/java/shibafu/yukari/core/YukariApplication.java
+++ Yukari/src/main/java/shibafu/yukari/core/YukariApplication.java
@@ -2,6 +2,7 @@ package shibafu.yukari.core;
 
 import android.app.Application;
 import android.preference.PreferenceManager;
+import twitter4j.AlternativeHttpClientImpl;
 
 /**
  * Created by shibafu on 2015/08/29.
@@ -15,6 +16,11 @@ public class YukariApplication extends Application {
         if (PreferenceManager.getDefaultSharedPreferences(this).getBoolean("pref_disable_ipv6", false)) {
             java.lang.System.setProperty("java.net.preferIPv4Stack", "true");
             java.lang.System.setProperty("java.net.preferIPv6Addresses", "false");
+        }
+
+        if (PreferenceManager.getDefaultSharedPreferences(this).getBoolean("pref_force_http1", false)) {
+            AlternativeHttpClientImpl.sPreferHttp2 = false;
+            AlternativeHttpClientImpl.sPreferSpdy = false;
         }
     }
 }
