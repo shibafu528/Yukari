45e6c8e98a24b15ebbb9a2469071602dc749ea81
shibafu <shibafu528@gmail.com>
2017-01-05 00:33:00 +0900

    設定のカテゴリ化

--- Yukari/src/main/java/shibafu/yukari/activity/ConfigActivity.java
+++ Yukari/src/main/java/shibafu/yukari/activity/ConfigActivity.java
@@ -9,6 +9,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
 import com.github.machinarius.preferencefragment.PreferenceFragment;
@@ -26,9 +27,11 @@ public class ConfigActivity extends ActionBarYukariBase {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.frame, new InnerFragment())
-                .commit();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.frame, InnerFragment.newInstance(getIntent().getStringExtra("category")))
+                    .commit();
+        }
     }
 
     @Override
@@ -46,92 +49,157 @@ public class ConfigActivity extends ActionBarYukariBase {
     public void onServiceDisconnected() {}
 
     public static class InnerFragment extends PreferenceFragment {
+        public static InnerFragment newInstance(String category) {
+            InnerFragment fragment = new InnerFragment();
+            Bundle args = new Bundle();
+            args.putString("category", category);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
         @Override
         public void onCreate(Bundle paramBundle) {
             super.onCreate(paramBundle);
-            addPreferencesFromResource(R.xml.pref);
+            Bundle args = getArguments();
+            String category = args.getString("category");
+            if (TextUtils.isEmpty(category)) {
+                addPreferencesFromResource(R.xml.pref);
+            } else {
+                switch (category) {
+                    case "appearance":
+                        getActivity().setTitle("表示設定");
+                        addPreferencesFromResource(R.xml.pref_appearance);
+                        break;
+                    case "function":
+                        getActivity().setTitle("機能設定");
+                        addPreferencesFromResource(R.xml.pref_function);
+                        break;
+                    case "post":
+                        getActivity().setTitle("投稿設定");
+                        addPreferencesFromResource(R.xml.pref_post);
+                        break;
+                    case "filter":
+                        getActivity().setTitle("フィルタ設定");
+                        addPreferencesFromResource(R.xml.pref_filter);
+                        break;
+                    case "dialog":
+                        getActivity().setTitle("操作確認設定");
+                        addPreferencesFromResource(R.xml.pref_dialog);
+                        break;
+                    case "notify":
+                        getActivity().setTitle("通知設定");
+                        addPreferencesFromResource(R.xml.pref_notify);
+                        break;
+                    case "search":
+                        getActivity().setTitle("検索設定");
+                        addPreferencesFromResource(R.xml.pref_search);
+                        break;
+                    case "expert":
+                        getActivity().setTitle("詳細設定");
+                        addPreferencesFromResource(R.xml.pref_expert);
+                        break;
+                    case "plugin":
+                        getActivity().setTitle("プラグインエンジン (実験中)");
+                        addPreferencesFromResource(R.xml.pref_plugin);
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Illegal category extra: " + category);
+                }
+            }
         }
 
         @Override
         public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
             super.onViewCreated(view, savedInstanceState);
 
-            findPreference("pref_boot_immersive").setEnabled(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT);
+            Bundle args = getArguments();
+            String category = args.getString("category");
+            if (TextUtils.isEmpty(category)) {
+                {
+                    StringBuilder summaryText = new StringBuilder();
+                    PackageManager pm = getActivity().getPackageManager();
+                    // Yukari version
+                    try {
+                        PackageInfo packageInfo = pm.getPackageInfo(getActivity().getPackageName(), 0);
+                        summaryText.append("Version ").append(packageInfo.versionName);
+                    } catch (PackageManager.NameNotFoundException ignored) {}
+                    // Developer
+                    summaryText.append("\nDeveloped by @shibafu528");
+                    findPreference("pref_about_version").setSummary(summaryText.toString());
+                }
 
-            {
-                String summaryText = "libexvoice.so (" + BuildInfo.getABI() + ") Build: " + BuildInfo.getBuildDateTime() +
-                        "\n  with " + BuildInfo.getMRubyDescription() + ",\n    " + BuildInfo.getMRubyCopyright();
-                // exvoice version
-                findPreference("pref_exvoice_version").setSummary(summaryText);
-            }
-
-            {
-                StringBuilder summaryText = new StringBuilder();
-                PackageManager pm = getActivity().getPackageManager();
-                // Yukari version
-                try {
-                    PackageInfo packageInfo = pm.getPackageInfo(getActivity().getPackageName(), 0);
-                    summaryText.append("Version ").append(packageInfo.versionName);
-                } catch (PackageManager.NameNotFoundException ignored) {}
-                // Developer
-                summaryText.append("\nDeveloped by @shibafu528");
-                findPreference("pref_about_version").setSummary(summaryText.toString());
-            }
-
-            findPreference("pref_about_feedback").setOnPreferenceClickListener(preference -> {
-                Intent intent = new Intent(getActivity(), TweetActivity.class);
-                String text = " #yukari4a @yukari4a";
-                PackageManager pm = getActivity().getPackageManager();
-                try {
-                    PackageInfo packageInfo = pm.getPackageInfo(getActivity().getPackageName(), 0);
-                    String[] versionText = packageInfo.versionName.split(" ");
-                    if (versionText.length > 1) {
-                        text += " //ver." + versionText[0];
+                findPreference("pref_about_feedback").setOnPreferenceClickListener(preference -> {
+                    Intent intent = new Intent(getActivity(), TweetActivity.class);
+                    String text = " #yukari4a @yukari4a";
+                    PackageManager pm = getActivity().getPackageManager();
+                    try {
+                        PackageInfo packageInfo = pm.getPackageInfo(getActivity().getPackageName(), 0);
+                        String[] versionText = packageInfo.versionName.split(" ");
+                        if (versionText.length > 1) {
+                            text += " //ver." + versionText[0];
+                        }
+                    } catch (PackageManager.NameNotFoundException e) {
+                        e.printStackTrace();
                     }
-                } catch (PackageManager.NameNotFoundException e) {
-                    e.printStackTrace();
-                }
-                intent.putExtra(TweetActivity.EXTRA_TEXT, text);
-                startActivity(intent);
-                return true;
-            });
+                    intent.putExtra(TweetActivity.EXTRA_TEXT, text);
+                    startActivity(intent);
+                    return true;
+                });
 
-            findPreference("pref_prev_time").setOnPreferenceClickListener(preference -> {
-                final SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
-                int selectedFlags = sp.getInt("pref_prev_time", 0);
-                final boolean[] selectedStates = new boolean[24];
-                for (int i = 0; i < 24; ++i) {
-                    selectedStates[i] = (selectedFlags & 0x01) == 1;
-                    selectedFlags >>>= 1;
-                }
-                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
-                        .setTitle("サムネイル非表示にする時間帯")
-                        .setPositiveButton("OK", (dialog, which) -> {
-                            int selectedFlags1 = 0;
-                            for (int i = 23; i >= 0; --i) {
-                                selectedFlags1 <<= 1;
-                                selectedFlags1 |= selectedStates[i] ? 1 : 0;
+            } else {
+                switch (category) {
+                    case "appearance":
+                        findPreference("pref_boot_immersive").setEnabled(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT);
+
+                        findPreference("pref_prev_time").setOnPreferenceClickListener(preference -> {
+                            final SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
+                            int selectedFlags = sp.getInt("pref_prev_time", 0);
+                            final boolean[] selectedStates = new boolean[24];
+                            for (int i = 0; i < 24; ++i) {
+                                selectedStates[i] = (selectedFlags & 0x01) == 1;
+                                selectedFlags >>>= 1;
                             }
-                            sp.edit().putInt("pref_prev_time", selectedFlags1).commit();
-                        })
-                        .setNegativeButton("キャンセル", (dialog, which) -> {
-                        })
-                        .setMultiChoiceItems(R.array.pref_prev_time_entries, selectedStates, (dialog, which, isChecked) -> {
-                            selectedStates[which] = isChecked;
+                            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
+                                    .setTitle("サムネイル非表示にする時間帯")
+                                    .setPositiveButton("OK", (dialog, which) -> {
+                                        int selectedFlags1 = 0;
+                                        for (int i = 23; i >= 0; --i) {
+                                            selectedFlags1 <<= 1;
+                                            selectedFlags1 |= selectedStates[i] ? 1 : 0;
+                                        }
+                                        sp.edit().putInt("pref_prev_time", selectedFlags1).commit();
+                                    })
+                                    .setNegativeButton("キャンセル", (dialog, which) -> {
+                                    })
+                                    .setMultiChoiceItems(R.array.pref_prev_time_entries, selectedStates, (dialog, which, isChecked) -> {
+                                        selectedStates[which] = isChecked;
+                                    });
+                            builder.create().show();
+                            return true;
                         });
-                builder.create().show();
-                return true;
-            });
+                        break;
 
-            findPreference("pref_export").setOnPreferenceClickListener(preference -> {
-                startActivity(new Intent(getActivity(), BackupActivity.class).putExtra(BackupActivity.EXTRA_MODE, BackupActivity.EXTRA_MODE_EXPORT));
-                return false;
-            });
+                    case "expert":
+                        findPreference("pref_export").setOnPreferenceClickListener(preference -> {
+                            startActivity(new Intent(getActivity(), BackupActivity.class).putExtra(BackupActivity.EXTRA_MODE, BackupActivity.EXTRA_MODE_EXPORT));
+                            return false;
+                        });
 
-            findPreference("pref_import").setOnPreferenceClickListener(preference -> {
-                startActivity(new Intent(getActivity(), BackupActivity.class).putExtra(BackupActivity.EXTRA_MODE, BackupActivity.EXTRA_MODE_IMPORT));
-                return false;
-            });
+                        findPreference("pref_import").setOnPreferenceClickListener(preference -> {
+                            startActivity(new Intent(getActivity(), BackupActivity.class).putExtra(BackupActivity.EXTRA_MODE, BackupActivity.EXTRA_MODE_IMPORT));
+                            return false;
+                        });
+                        break;
+
+                    case "plugin": {
+                        String summaryText = "libexvoice.so (" + BuildInfo.getABI() + ") Build: " + BuildInfo.getBuildDateTime() +
+                                "\n  with " + BuildInfo.getMRubyDescription() + ",\n    " + BuildInfo.getMRubyCopyright();
+                        // exvoice version
+                        findPreference("pref_exvoice_version").setSummary(summaryText);
+                        break;
+                    }
+                }
+            }
         }
     }
 }
