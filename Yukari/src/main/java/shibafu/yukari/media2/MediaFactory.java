747bcfc916c67f1355928a3c4b461a79268f03ec
shibafu <shibafu528@gmail.com>
2017-09-18 23:47:51 +0900

    新実装にもインスタンスキャッシュを移植

--- Yukari/src/main/java/shibafu/yukari/media2/MediaFactory.java
+++ Yukari/src/main/java/shibafu/yukari/media2/MediaFactory.java
@@ -1,17 +1,30 @@
 package shibafu.yukari.media2;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class MediaFactory {
+    private static Map<String, Media> instanceQueue = new LinkedHashMap<String, Media>() {
+        @Override
+        protected boolean removeEldestEntry(Entry eldest) {
+            return size() > 32;
+        }
+    };
 
     public static Media newInstance(String browseUrl) {
-        Media media = null;
+        Media media = instanceQueue.get(browseUrl);
 
-        if (browseUrl.contains("pbs.twimg.com/media/")) {
-            media = new Twimg(browseUrl);
-        } else if (browseUrl.contains("pixiv.net/member_illust.php")) {
-            media = new Pixiv(browseUrl);
-        } else if (browseUrl.endsWith(".jpg") || browseUrl.endsWith(".jpeg") || browseUrl.endsWith(".png")) {
-            media = new SimplePicture(browseUrl);
+        if (media == null) {
+            if (browseUrl.contains("pbs.twimg.com/media/")) {
+                media = new Twimg(browseUrl);
+            } else if (browseUrl.contains("pixiv.net/member_illust.php")) {
+                media = new Pixiv(browseUrl);
+            } else if (browseUrl.endsWith(".jpg") || browseUrl.endsWith(".jpeg") || browseUrl.endsWith(".png")) {
+                media = new SimplePicture(browseUrl);
+            }
         }
+
+        instanceQueue.put(browseUrl, media);
 
         return media;
     }
