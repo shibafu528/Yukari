d350373d989682fd234c898e482cc1854325db01
shibafu <shibafu528@gmail.com>
2018-04-09 21:54:58 +0900

    mumei_himazin/twitter-intent サポートの削除

--- Yukari/src/main/java/shibafu/yukari/plugin/OpenAclogActivity.java
+++ Yukari/src/main/java/shibafu/yukari/plugin/OpenAclogActivity.java
@@ -5,10 +5,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
-import twitter.intent.TwitterIntent;
-import twitter.intent.TwitterShare;
-import twitter.intent.StatusIntent;
-
 /**
  * Created by shibafu on 14/03/27.
  */
@@ -18,16 +14,7 @@ public class OpenAclogActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
-        String id;
-        switch (intent.getAction()) {
-            case TwitterIntent.ACTION_SHOW_STATUS:
-                StatusIntent statusIntent = (StatusIntent) TwitterShare.getTwitterIntent(intent);
-                id = String.valueOf(statusIntent.getId());
-                break;
-            default:
-                id = intent.getStringExtra("id");
-                break;
-        }
+        String id = intent.getStringExtra("id");
 
         Uri uri = Uri.withAppendedPath(Uri.parse("http://aclog.rhe.jp/i"), id);
         startActivity(new Intent(Intent.ACTION_VIEW, uri));
