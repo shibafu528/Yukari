65b0d803a3b4b474472f8fa264acf9758526bac5
shibafu <shibafu528@gmail.com>
2018-08-12 15:46:23 +0900

    サウンドテーマAkari,KiriのRT音ファイル名指定ミスを修正
    refs #151

--- Yukari/src/main/java/shibafu/yukari/twitter/statusmanager/StatusNotifier.java
+++ Yukari/src/main/java/shibafu/yukari/twitter/statusmanager/StatusNotifier.java
@@ -95,12 +95,12 @@ class StatusNotifier implements Releasable {
         SE_URIS.put("akari_fav", new SoundEffects() {{
             reply = Uri.parse("android.resource://shibafu.yukari/raw/akari_reply");
             favorite = Uri.parse("android.resource://shibafu.yukari/raw/akari_fav");
-            retweet = Uri.parse("android.resource://shibafu.yukari/raw/akari_rt");
+            retweet = Uri.parse("android.resource://shibafu.yukari/raw/akari_retweet");
         }});
         SE_URIS.put("akari_like", new SoundEffects() {{
             reply = Uri.parse("android.resource://shibafu.yukari/raw/akari_reply");
             favorite = Uri.parse("android.resource://shibafu.yukari/raw/akari_like");
-            retweet = Uri.parse("android.resource://shibafu.yukari/raw/akari_rt");
+            retweet = Uri.parse("android.resource://shibafu.yukari/raw/akari_retweet");
         }});
         SE_URIS.put("akari_love", new SoundEffects() {{
             reply = Uri.parse("android.resource://shibafu.yukari/raw/akari_reply");
@@ -110,7 +110,7 @@ class StatusNotifier implements Releasable {
         SE_URIS.put("kiri_like", new SoundEffects() {{
             reply = Uri.parse("android.resource://shibafu.yukari/raw/kiri_reply");
             favorite = Uri.parse("android.resource://shibafu.yukari/raw/kiri_like");
-            retweet = Uri.parse("android.resource://shibafu.yukari/raw/krtn_rt");
+            retweet = Uri.parse("android.resource://shibafu.yukari/raw/kiri_retweet");
         }});
         SE_URIS.put("kiri_love", new SoundEffects() {{
             reply = Uri.parse("android.resource://shibafu.yukari/raw/kiri_reply");
