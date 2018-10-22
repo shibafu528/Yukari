583f64ffa7f1efac80c041ae9cdb495ce6ac2c38
shibafu <shibafu528@gmail.com>
2018-10-22 22:28:09 +0900

    ニコ動、https化したってよ

--- Yukari/src/main/java/shibafu/yukari/media2/impl/NicoVideo.java
+++ Yukari/src/main/java/shibafu/yukari/media2/impl/NicoVideo.java
@@ -8,7 +8,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class NicoVideo extends MemoizeMedia {
-    private static final Pattern URL_PATTERN = Pattern.compile("http://(?:(?:www|sp)\\.nicovideo\\.jp/watch|nico\\.ms)/[sn][mo]([1-9]\\d*)");
+    private static final Pattern URL_PATTERN = Pattern.compile("https?://(?:(?:www|sp)\\.nicovideo\\.jp/watch|nico\\.ms)/[sn][mo]([1-9]\\d*)");
 
     /**
      * @param browseUrl メディアの既知のURL
