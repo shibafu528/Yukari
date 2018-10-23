ccd26fabfb258108bd614f19449908f57cfeef82
shibafu <shibafu528@gmail.com>
2018-10-23 20:29:54 +0900

    プロフィール画面が非対応URLを受けた場合、ACTION_VIEWでIntentを再発行して終了するようにした

--- Yukari/src/main/java/shibafu/yukari/activity/ProfileActivity.java
+++ Yukari/src/main/java/shibafu/yukari/activity/ProfileActivity.java
@@ -11,6 +11,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.widget.Toast;
 import shibafu.yukari.R;
@@ -58,21 +59,30 @@ public class ProfileActivity extends AppCompatActivity {
             actionBar.hide();
 
             Intent intent = getIntent();
-            if (intent.getData() != null && intent.getData().getLastPathSegment() == null) {
+            Uri uri = intent.getData();
+            if (uri == null) {
                 Toast.makeText(this, "エラー: 無効なユーザ指定です", Toast.LENGTH_LONG).show();
                 finish();
                 return;
             }
 
-            ProfileFragment fragment = new ProfileFragment();
-            Bundle b = new Bundle();
-            b.putSerializable(ProfileFragment.EXTRA_USER, intent.getSerializableExtra(EXTRA_USER));
-            b.putLong(ProfileFragment.EXTRA_TARGET, intent.getLongExtra(EXTRA_TARGET, -1));
-            b.putParcelable("data", intent.getData());
-            fragment.setArguments(b);
+            if ("twitter.com".equals(uri.getHost()) && !TextUtils.isEmpty(uri.getLastPathSegment())) {
+                ProfileFragment fragment = new ProfileFragment();
+                Bundle b = new Bundle();
+                b.putSerializable(ProfileFragment.EXTRA_USER, intent.getSerializableExtra(EXTRA_USER));
+                b.putLong(ProfileFragment.EXTRA_TARGET, intent.getLongExtra(EXTRA_TARGET, -1));
+                b.putParcelable("data", intent.getData());
+                fragment.setArguments(b);
 
-            FragmentTransaction ft = manager.beginTransaction();
-            ft.replace(R.id.frame, fragment, "contain").commit();
+                FragmentTransaction ft = manager.beginTransaction();
+                ft.replace(R.id.frame, fragment, "contain").commit();
+            } else {
+                // 非対応ホストの場合は他のアプリに頑張ってもらう
+                Intent newIntent = new Intent(Intent.ACTION_VIEW, uri);
+                newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(newIntent);
+                finish();
+            }
         }
     }
 
