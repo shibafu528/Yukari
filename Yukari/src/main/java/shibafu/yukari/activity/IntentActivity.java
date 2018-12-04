a1fcf22211edba1d8cda4103ecc422cb4c645059
shibafu <shibafu528@gmail.com>
2018-12-05 00:21:35 +0900

    Android 6.0以降で、onCreateで完結しないActivityでTheme.NoDisplayを使うとクラッシュする仕様の回避

--- Yukari/src/main/java/shibafu/yukari/activity/IntentActivity.java
+++ Yukari/src/main/java/shibafu/yukari/activity/IntentActivity.java
@@ -3,9 +3,11 @@ package shibafu.yukari.activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Pair;
 import android.widget.Toast;
-import shibafu.yukari.activity.base.ListYukariBase;
+import shibafu.yukari.R;
+import shibafu.yukari.activity.base.ActionBarYukariBase;
 import shibafu.yukari.common.TweetDraft;
 import shibafu.yukari.common.async.ParallelAsyncTask;
 import shibafu.yukari.service.PostService;
@@ -23,7 +25,7 @@ import java.util.regex.Pattern;
 /**
  * Created by Shibafu on 13/08/09.
  */
-public class IntentActivity extends ListYukariBase{
+public class IntentActivity extends ActionBarYukariBase {
 
     private static final List<Pair<Pattern, AfterWork>> MATCHES;
     static {
@@ -83,7 +85,12 @@ public class IntentActivity extends ListYukariBase{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        if (PreferenceManager.getDefaultSharedPreferences(this).getString("pref_theme", "light").endsWith("dark")) {
+            setTheme(R.style.ColorsTheme_Dark_Translucent_FullTransparent);
+        } else {
+            setTheme(R.style.ColorsTheme_Light_Translucent_FullTransparent);
+        }
+        super.onCreate(savedInstanceState, true);
 
         String url = getIntent().getDataString();
         for (Pair<Pattern, AfterWork> pair : MATCHES) {
