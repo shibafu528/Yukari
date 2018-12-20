1876d085c3d9e3b0cbf28ec6a3d93a9ca506eaac
shibafu <shibafu528@gmail.com>
2018-12-21 01:33:21 +0900

    アプリ全体レベルの通知チャンネルを定義

--- Yukari/src/main/java/shibafu/yukari/core/YukariApplication.java
+++ Yukari/src/main/java/shibafu/yukari/core/YukariApplication.java
@@ -1,12 +1,16 @@
 package shibafu.yukari.core;
 
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.os.Build;
 import android.preference.PreferenceManager;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.security.ProviderInstaller;
 import com.squareup.leakcanary.LeakCanary;
+import shibafu.yukari.R;
 import twitter4j.AlternativeHttpClientImpl;
 
 /**
@@ -30,6 +34,35 @@ public class YukariApplication extends Application {
         if (PreferenceManager.getDefaultSharedPreferences(this).getBoolean("pref_force_http1", false)) {
             AlternativeHttpClientImpl.sPreferHttp2 = false;
             AlternativeHttpClientImpl.sPreferSpdy = false;
+        }
+
+        // 通知チャンネルの作成
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+            NotificationChannel generalChannel = new NotificationChannel(
+                    getString(R.string.notification_channel_id_general),
+                    "その他の通知",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            nm.createNotificationChannel(generalChannel);
+
+            NotificationChannel errorChannel = new NotificationChannel(
+                    getString(R.string.notification_channel_id_error),
+                    "エラーの通知",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            nm.createNotificationChannel(errorChannel);
+
+            NotificationChannel coreServiceChannel = new NotificationChannel(
+                    getString(R.string.notification_channel_id_core_service),
+                    "常駐サービス",
+                    NotificationManager.IMPORTANCE_MIN);
+            nm.createNotificationChannel(coreServiceChannel);
+
+            NotificationChannel asyncActionChannel = new NotificationChannel(
+                    getString(R.string.notification_channel_id_async_action),
+                    "バックグラウンド処理",
+                    NotificationManager.IMPORTANCE_LOW);
+            nm.createNotificationChannel(asyncActionChannel);
         }
 
         if (LeakCanary.isInAnalyzerProcess(this)) {
