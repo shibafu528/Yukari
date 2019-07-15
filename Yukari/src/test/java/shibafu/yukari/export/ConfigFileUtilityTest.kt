55a2e246f2d65007da148a2a98c8f7616f78d48e
shibafu <shibafu528@gmail.com>
2019-07-15 18:41:09 +0900

    テストを直した

--- Yukari/src/test/java/shibafu/yukari/export/ConfigFileUtilityTest.kt
+++ Yukari/src/test/java/shibafu/yukari/export/ConfigFileUtilityTest.kt
@@ -2,8 +2,13 @@ package shibafu.yukari.export
 
 import org.junit.BeforeClass
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import shibafu.yukari.database.CentralDatabase
 import kotlin.test.assertEquals
 
+@RunWith(RobolectricTestRunner::class)
 class ConfigFileUtilityTest {
     companion object {
         @BeforeClass @JvmStatic fun beforeClass() {
@@ -25,7 +30,7 @@ class ConfigFileUtilityTest {
 
     @Test fun importFromJsonTest() {
         val json = """{"version":2,"ConfigTestEntity":[{"str":"abcde","num":114514,"num2":1919810,"numstr":"114514"}]}"""
-        val records = ConfigFileUtility.importFromJson(ConfigTestEntity::class.java, json)
+        val records = ConfigFileUtility.importFromJson(ConfigTestEntity::class.java, json, CentralDatabase(RuntimeEnvironment.application))
         assertEquals(1, records.size)
         assertEquals("abcde", records.first()["str"])
         assertEquals(114514.0, records.first()["num"])
@@ -35,7 +40,7 @@ class ConfigFileUtilityTest {
 
     @Test fun importFromJsonMigrateTest() {
         val json = """{"version":1,"ConfigTestEntity":[{"strold":"abcde","num":114514}]}"""
-        val records = ConfigFileUtility.importFromJson(ConfigTestEntity::class.java, json)
+        val records = ConfigFileUtility.importFromJson(ConfigTestEntity::class.java, json, CentralDatabase(RuntimeEnvironment.application))
         assertEquals(1, records.size)
         assertEquals("abcde", records.first()["str"])
         assertEquals(114514.0, records.first()["num"])
@@ -45,17 +50,17 @@ class ConfigFileUtilityTest {
 
     @Test fun importFromRenamedJsonTest() {
         val json1 = """{"version":1,"ConfigRenameTestEntityOld":[{"str":"abcde"}]}"""
-        val records1 = ConfigFileUtility.importFromJson(ConfigRenameTestEntity::class.java, json1)
+        val records1 = ConfigFileUtility.importFromJson(ConfigRenameTestEntity::class.java, json1, CentralDatabase(RuntimeEnvironment.application))
         assertEquals(1, records1.size)
         assertEquals("abcde", records1.first()["str"])
 
         val json2 = """{"version":2,"ConfigRenameTestEntityOld":[{"str":"abcde"}]}"""
-        val records2 = ConfigFileUtility.importFromJson(ConfigRenameTestEntity::class.java, json2)
+        val records2 = ConfigFileUtility.importFromJson(ConfigRenameTestEntity::class.java, json2, CentralDatabase(RuntimeEnvironment.application))
         assertEquals(1, records2.size)
         assertEquals("abcde", records1.first()["str"])
 
         val json3 = """{"version":3,"ConfigRenameTestEntity":[{"str":"abcde"}]}"""
-        val records3 = ConfigFileUtility.importFromJson(ConfigRenameTestEntity::class.java, json3)
+        val records3 = ConfigFileUtility.importFromJson(ConfigRenameTestEntity::class.java, json3, CentralDatabase(RuntimeEnvironment.application))
         assertEquals(1, records3.size)
         assertEquals("abcde", records1.first()["str"])
     }
@@ -78,13 +83,13 @@ class TestMigrator : ConfigFileMigrator<ConfigTestEntity> {
 
     constructor() : super(ConfigTestEntity::class.java, {
         // version 1 -> 2
-        migrateTo(2) {
+        migrateTo(2) { it, _ ->
             it["num2"] = it["num"]
             it["str"] = it["strold"]
             it["numstr"] = (it["num"] as Number).toInt().toString()
         }
         // version 2 -> 3
-        migrateTo(3) {
+        migrateTo(3) { it, _ ->
             it.remove("str")
             it.remove("num2")
         }
