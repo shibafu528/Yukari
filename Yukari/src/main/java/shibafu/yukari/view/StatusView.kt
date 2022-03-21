4bd55c3a0b142d0d2406b59e31596605c0ec27cb
shibafu <shibafu528@gmail.com>
2022-03-21 18:29:35 +0900

    TL上のサムネタップ時にプレビュー非対応のメディアでも画像プレビューを開いていたバグの修正

--- Yukari/src/main/java/shibafu/yukari/view/StatusView.kt
+++ Yukari/src/main/java/shibafu/yukari/view/StatusView.kt
@@ -450,7 +450,7 @@ abstract class StatusView : RelativeLayout {
                                 BitmapCache.IMAGE_CACHE,
                                 hidden && pref.getBoolean("pref_prev_mosaic", false))
 
-                        if (mode and Mode.DETAIL == Mode.DETAIL && media.canPreview() || pref.getBoolean("pref_extended_touch_event", false)) {
+                        if ((mode and Mode.DETAIL == Mode.DETAIL || pref.getBoolean("pref_extended_touch_event", false)) && media.canPreview()) {
                             iv.setOnClickListener { _ ->
                                 val intent = PreviewActivity2.newIntent(context, Uri.parse(media.browseUrl), status,
                                         collection = mediaList.filter { it.canPreview() }.map { Uri.parse(it.browseUrl) })
